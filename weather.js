93cd386dad8114f0c4d1b2616744c13bd61b061c
Andreas Jørgensen <ah.joergensen90@gmail.com>
2025-02-04 07:41:26 +0100

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 
-const apiKey = "//Your API Key//";
+const apiKey = "YOUR_API_KEY_HERE";
 
 weatherForm.addEventListener("submit", async event => {
 
